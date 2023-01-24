f3b2311c12870e755036593860d9fa4ca86665d8
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-01-24 20:34:24 +0000

    Add functionality to display numbers when pressed

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+let buttons = document.querySelectorAll('button');
+let displayValue= '';
+buttons.forEach((b)=>{
+   b.addEventListener('click', function(){
+
+      if(this.classList.value !== "operator"){
+         populateDisplay(this.id);
+      }
+   
+   });
+});
+
+
+
+function populateDisplay(buttonID)
+{
+   displayValue += buttonID;
+   document.querySelector("#display").textContent = displayValue;
+}
 
 function add()
 {
